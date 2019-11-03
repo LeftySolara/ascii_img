c98a1b8ad16355e638a02adf6d0b359370e3ef2e
Jalen Adams <jalen@jalenkadams.me>
2019-11-03 14:50:34 -0500

    Add: get rgb pixel data

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-#include <stdio.h>
+#include <iostream>
 #include <opencv4/opencv2/opencv.hpp>
 
 using namespace cv;
+using std::cout;
+using std::endl;
 
 int main(int argc, char **argv)
 {
@@ -18,8 +20,23 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    printf("Successfully loaded image!\n");
-    printf("Image size: %d x %d\n", image.cols, image.rows);
+    int cols = image.cols;
+    int rows = image.rows;
+
+    cout << "Successfully loaded image!" << endl;
+    cout << "Image size: " << cols << " x " << rows << endl;
+
+    // If the image matrix is continuous, it's less overhead
+    // to process it as a single, long row.
+    if (image.isContinuous()) {
+        cols *= rows;
+        rows = 1;
+    }
+    for (int i = 0; i < rows; ++i) {
+        const Vec3b *pixels = image.ptr<Vec3b>(i);
+        for (int j = 0; j < cols; ++j)
+            cout << pixels[j] << endl;
+    }
 
     return 0;
 }
