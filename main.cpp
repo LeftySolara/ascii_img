c6eeae41e0a1a9b34e147421d6c7e412e0fe18cb
Jalen Adams <jalen@jalenkadams.me>
2019-11-03 14:18:11 -0500

    Add: Get image dimensions

--- main.cpp
+++ main.cpp
@@ -5,6 +5,21 @@ using namespace cv;
 
 int main(int argc, char **argv)
 {
-    printf("Hello world!\n");
+    if (argc != 2) {
+        printf("usage: AsciiArt <ImagePath>\n");
+        return -1;
+    }
+
+    Mat image;
+    image = imread(argv[1], 1);
+
+    if (!image.data) {
+        printf("No image data\n");
+        return -1;
+    }
+
+    printf("Successfully loaded image!\n");
+    printf("Image size: %d x %d\n", image.cols, image.rows);
+
     return 0;
 }
