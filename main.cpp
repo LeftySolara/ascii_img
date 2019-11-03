54f1049c93aa93b906c1a29576acffe077bde4df
Jalen Adams <jalen@jalenkadams.me>
2019-11-03 16:35:06 -0500

    Add: function to iterate over pixel brightness values

--- main.cpp
+++ main.cpp
@@ -5,6 +5,36 @@ using namespace cv;
 using std::cout;
 using std::endl;
 
+Mat loadImage(char *path)
+{
+    Mat image = imread(path, IMREAD_COLOR);
+    
+    if (!image.data) {
+        cout << "Error: could not load image" << endl;
+        exit(-1);
+    }
+    cout << "Successfully loaded image!" << endl;
+    cout << "Image size: " << image.cols << " x " << image.rows << endl;
+    
+    return image;
+}
+
+void iterateBrightness(Mat image)
+{
+    int rows = image.rows;
+    int cols = image.cols;
+    int avg;
+
+    cout << "Iterating through pixel brightnesses:" << endl;
+    for (int y = 0; y < rows; ++y) {
+        for (int x = 0; x < cols; ++x) {
+            const Vec3b pixel = image.at<Vec3b>(y, x);
+            avg = (pixel.val[0] + pixel.val[1] + pixel.val[2]) / 3;
+            cout << avg << endl;
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2) {
@@ -12,31 +42,8 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    Mat image;
-    image = imread(argv[1], 1);
-
-    if (!image.data) {
-        printf("No image data\n");
-        return -1;
-    }
-
-    int cols = image.cols;
-    int rows = image.rows;
-
-    cout << "Successfully loaded image!" << endl;
-    cout << "Image size: " << cols << " x " << rows << endl;
-
-    // If the image matrix is continuous, it's less overhead
-    // to process it as a single, long row.
-    if (image.isContinuous()) {
-        cols *= rows;
-        rows = 1;
-    }
-    for (int i = 0; i < rows; ++i) {
-        const Vec3b *pixels = image.ptr<Vec3b>(i);
-        for (int j = 0; j < cols; ++j)
-            cout << pixels[j] << endl;
-    }
+    Mat image = loadImage(argv[1]);
+    iterateBrightness(image);
 
     return 0;
 }
