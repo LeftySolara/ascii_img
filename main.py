1b6caa0c24d3a944218149c6f64ea9bf4386e861
Jalen Adams <jalen@jalenkadams.me>
2019-11-05 22:48:05 -0500

    Create ascii matrix

--- main.py
+++ main.py
@@ -23,6 +23,16 @@ def main():
 
     print("Successfully created brightness matrix")
 
+    ascii_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+    ascii_matrix = numpy.zeros((image.height, image.width), dtype=str)
+
+    for y in range(0, image.height):
+        for x in range(0, image.width):
+            char_index = (brightness_matrix[y][x] / 255) * len(ascii_chars)
+            ascii_matrix[y][x] = ascii_chars[int(char_index)]
+
+    print("Successfully created ASCII matrix")
+
 
 if __name__ == "__main__":
     main()
