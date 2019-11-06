eb17e8577a4f0bc04a3f676a34cba369e3ff41a0
Jalen Adams <jalen@jalenkadams.me>
2019-11-05 22:26:20 -0500

    Create brightness matrix

--- main.py
+++ main.py
@@ -8,16 +8,21 @@ def main():
         return
 
     image = Image.open(sys.argv[1])
-    print("Successfully loaded image!")
+    print("Successfully loaded image")
     print("Image size: {} x {}".format(image.width, image.height))
 
-    pixels = numpy.asarray(image)
+    pixels = numpy.asarray(image, numpy.intc)
+    print()
+    print("Successfully created pixel matrix")
 
-    print("Iterating through pixel contents:")
-    for y in range (0, image.height):
+    brightness_matrix = numpy.zeros((image.height, image.width), numpy.uint8)
+    for y in range(0, image.height):
         for x in range(0, image.width):
-            print(pixels[y][x])
-        print()
+            brightness = (pixels[y][x][0] + pixels[y][x][1] + pixels[y][x][2]) // 3
+            brightness_matrix[y][x] = brightness
+
+    print("Successfully created brightness matrix")
+
 
 if __name__ == "__main__":
     main()
